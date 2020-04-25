394f34126a25301ca39e6f967f3e03a192f85139
TheLevius <reflevius@gmail.com>
2020-04-25 11:38:27 +0300

    Updates

---
+++ precache-manifest.41bb2dcc9c5a98797f70ae3814d3dd58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c71dec91a87ad347673359ecf19e41b",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "f53a05f07783faa5c7cf",
+    "url": "/counter/static/css/main.d1191c67.chunk.css"
+  },
+  {
+    "revision": "f5653ca681e4dd05fdee",
+    "url": "/counter/static/js/2.0fdd1a41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.0fdd1a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53a05f07783faa5c7cf",
+    "url": "/counter/static/js/main.e8205f43.chunk.js"
+  },
+  {
+    "revision": "a3768dd25beead9ded19",
+    "url": "/counter/static/js/runtime-main.af1d9b19.js"
+  }
+]);
